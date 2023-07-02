9089eda657e9e885e39c9f8d433af0db47d4d3e7
María Puchades <100128850+emepuchades@users.noreply.github.com>
2023-07-02 20:11:04 +0200

    Feat: Added Top countries chart

--- src/utils/service.api.js
+++ src/utils/service.api.js
@@ -1,4 +1,5 @@
 import axios from "./backend.config.js";
+import abbreviations from "./abbreviations.json";
 
 export const getInternetUsers = async () => {
   const startYear = 1990;
@@ -29,30 +30,53 @@ export const getInternetUsers = async () => {
   }
 };
 
-export const getInternetUsersAndYear = async () => {
-  const startYear = 1990;
-  const internetUsersYear = [];
+export const getInternetUsersAndYear = async (pageNumber, pageSize) => {
+  const startYear = 2000;
+  const endYear = 2020;
   const countries = await getCountries();
+  const internetUsersYear = [];
+  const chunkSize = 10;
+  const smallArrays = [];
 
   try {
-    for (let year = startYear; year < 2020; year++) {
+    for (let year = startYear; year <= endYear; year++) {
       const response = await axios.get(`/year/${year}`);
       const yearData = {
-        year: year,
-        countries: [],
+        id: year.toString(),
+        data: [],
       };
 
-      for (let i = 0; i < 213; i++) {
-        const countryData = response.data.Data[countries[i]];
+      for (let i = 0; i < countries.length; i++) {
+        const country = countries[i];
+        const countryData = response.data.Data[country];
         const internetUsers = countryData?.internet_users_number || 0;
-        yearData.countries.push({
-          country: countries[i],
-          internetUsers: internetUsers,
-        });
+        if (i === 10) {
+          break;
+        }
+        if (internetUsers > 0) {
+          yearData.data.push({
+            x: country,
+            y: internetUsers,
+          });
+        } else {
+          yearData.data.push({
+            x: country,
+            y: 0,
+          });
+        }
       }
 
-      internetUsersYear.push(yearData);
+      if (yearData.data.length > 0) {
+        internetUsersYear.push(yearData);
+      }
     }
+
+    for (let i = 0; i < internetUsersYear.length; i += chunkSize) {
+      const chunk = internetUsersYear.slice(i, i + chunkSize);
+      smallArrays.push(chunk);
+    }
+
+    //console.log('smallArrays', smallArrays);
 
     return internetUsersYear;
   } catch (error) {
@@ -62,7 +86,79 @@ export const getInternetUsersAndYear = async () => {
 };
 
 export const topCountries = async () => {
-  
+  const startYear = 1990;
+  const endYear = 2020;
+  const countries = await getCountries();
+  //const countries = countriesResponse.Countries;
+  const internetUsersYear = [];
+
+  try {
+    for (let year = startYear; year <= endYear; year++) {
+      const response = await axios.get(`/year/${year}`);
+      const yearData = {
+        year: year,
+        countries: [],
+      };
+
+      const responseData = response.data.Data;
+
+      countries.forEach((country) => {
+        const countryData = responseData[country];
+
+        if (countryData && countryData.internet_users_number > 0) {
+          yearData.countries.push({
+            id: year,
+            label: country,
+            value: countryData.internet_users_number,
+            color: "hsl(149, 70%, 50%)",
+          });
+        }
+      });
+
+      // Ordenar los países por número de usuarios de Internet de forma descendente
+      yearData.countries.sort(
+        (a, b) => b.internet_users_number - a.internet_users_number
+      );
+
+      // Tomar solo los 10 países con más usuarios de Internet
+      yearData.countries = yearData.countries.slice(0, 10);
+
+      if (yearData.countries.length > 0) {
+        internetUsersYear.push(yearData);
+      }
+    }
+
+    return internetUsersYear;
+  } catch (error) {
+    console.error("Error al obtener los datos:", error);
+    throw new Error("Error al obtener los datos");
+  }
+};
+
+export const mapCountriesPerYear = async () => {
+  const response = await axios.get("/year/2020");
+  const data = response.data.Data;
+
+  // Obtén los pares clave-valor del objeto "data"
+  const formattedData = Object.keys(data).map((country) => {
+    // Obtén la abreviatura del país utilizando el archivo "abreviaturas.json"
+    const countryCode = Object.keys(abbreviations).find(
+      (key) => abbreviations[key] === country
+    );
+
+    //const countryCode = abbreviations[country.toUpperCase()];
+
+    // Obtén el número de usuarios de internet del país
+    const internetUsersNumber = data[country].internet_users_number;
+
+    // Retorna el objeto con el formato deseado
+    return {
+      id: countryCode,
+      value: internetUsersNumber,
+    };
+  });
+  console.log("formattedData", formattedData);
+  return formattedData;
 };
 
 export const getCountries = async () => {
