031a4e55c33ae4c6c7dd88b3df14a54eee38c387
María Puchades <100128850+emepuchades@users.noreply.github.com>
2023-07-02 09:34:30 +0200

    Feat: Internet Users x Year x Country

--- src/utils/service.api.js
+++ src/utils/service.api.js
@@ -1,15 +1,77 @@
 import axios from "./backend.config.js";
 
+export const getInternetUsers = async () => {
+  const startYear = 1990;
+  const internetUsersYear = [];
+  const countries = await getCountries();
+
+  try {
+    for (let year = startYear; year < 2021; year++) {
+      const response = await axios.get(`/year/${year}`);
+      let internetUsersTotal = 0;
+
+      for (let i = 0; i < 213; i++) {
+        const countryData = response.data.Data[countries[i]];
+        const internetUsers = countryData?.internet_users_number || 0;
+        internetUsersTotal += internetUsers;
+      }
+
+      internetUsersYear.push({
+        year: year,
+        total: internetUsersTotal,
+      });
+    }
+
+    return internetUsersYear;
+  } catch (error) {
+    console.error("Error al obtener los datos:", error);
+    throw new Error("Error al obtener los datos");
+  }
+};
+
+export const getInternetUsersAndYear = async () => {
+  const startYear = 1990;
+  const internetUsersYear = [];
+  const countries = await getCountries();
+
+  try {
+    for (let year = startYear; year < 2020; year++) {
+      const response = await axios.get(`/year/${year}`);
+      const yearData = {
+        year: year,
+        countries: [],
+      };
+
+      for (let i = 0; i < 213; i++) {
+        const countryData = response.data.Data[countries[i]];
+        const internetUsers = countryData?.internet_users_number || 0;
+        yearData.countries.push({
+          country: countries[i],
+          internetUsers: internetUsers,
+        });
+      }
+
+      internetUsersYear.push(yearData);
+    }
+
+    return internetUsersYear;
+  } catch (error) {
+    console.error("Error al obtener los datos:", error);
+    throw new Error("Error al obtener los datos");
+  }
+};
+
+export const topCountries = async () => {
+  
+};
+
 export const getCountries = async () => {
   try {
-     const response = await axios.get("/countries")
-     return response.data.Countries;
-    
+    const response = await axios.get("/countries");
+    return response.data.Countries;
   } catch (error) {
     throw new Error("Error al obtener los datos");
   }
 };
 
-
-
-export default getCountries;
+export default getInternetUsers;
